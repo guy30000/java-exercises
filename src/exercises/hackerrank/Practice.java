8b178ed15303d8e7d30a236514428efdd9514703
Spillz <billygcasey@hotmail.com>
2019-09-05 10:58:38 -0500

    Uploading some Hankerrank practices

---
+++ src/exercises/hackerrank/Practice.java
@@ -0,0 +1,19 @@
+package exercises.hackerrank;
+
+public class Practice {
+    private double accountBalance;
+
+    ///Getter n Setter
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public Practice (double balance){
+
+    }
+
+
+}
---
+++ src/exercises/hackerrank/Random.java
@@ -0,0 +1,51 @@
+package exercises.hackerrank;
+
+public class Random {
+
+    public static void main(String[] Args) {
+///SKipp Scanner
+//        Scanner scan = new Scanner(System.in);
+//        int i = scan.nextInt();
+//        Double d = scan.nextDouble();
+//        scan.nextLine();
+//        String s = scan.nextLine();
+
+//// between ints
+//        int N = scanner.nextInt();
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        if ((N % 2 == 1) || (6<=N && N <=20)){
+//            System.out.println("Weird");
+//        } else if ((2<=N&&N<=5) || (20<N)) {
+//            System.out.println("Not Weird");
+//        }
+
+        //Rounding
+//        double totCost=((meal_cost)+(meal_cost*tip_percent/100)+                        (meal_cost*tax_percent/100));
+//        System.out.println(Math.round(totCost));
+
+
+//sub arrays
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i; j < n; j++) {
+//                int sum = 0;
+//                for (int k = i; k <= j; k++) {
+//                    sum = sum + a[k];
+//                }
+//                if (sum < 0) {
+//                    numNegativeSubarrays++;
+//                }
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
