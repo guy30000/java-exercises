5ae7fd54c68926a9a958db4a87ce9b99751dbd91
Spillz <billygcasey@hotmail.com>
2019-09-18 19:22:54 -0500

    More tests

---
+++ src/exercises/hackerrank/Exception/Interface/StarWarsInterfacePractice/Character.java
@@ -0,0 +1,9 @@
+package exercises.hackerrank.Exception.Interface.StarWarsInterfacePractice;
+
+public interface Character {
+    public String base = "character";
+    public void attack();
+    public void heal();
+    public String getWeapon();
+
+}
