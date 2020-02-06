213046ccd01bf93dfb9165d7d278bfea0e46c52c
Spillz <billygcasey@hotmail.com>
2020-02-06 16:26:33 -0600

    Refactord Electronics Workshop to improve efficiency

--- src/exercises/hackerrank/Array_Play/Electronics_Shop.java
+++ src/exercises/hackerrank/Array_Play/Electronics_Shop.java
@@ -4,7 +4,7 @@ import java.math.*;
 import java.text.*;
 import java.util.*;
 import java.util.regex.*;
-
+//This one was fun because I figured it out but had to change it to save time. Had to refactor to better efficiency 
 
 public class Electronics_Shop {
 
@@ -16,43 +16,41 @@ public class Electronics_Shop {
         int[] totals = new int[drives.length * keyboards.length];
         int price = -1;
         int totPosition = 0;
-        int tempPrice = -1;
-        int tempPriceSet = 0;
-        int tempPriceSetLoop=0;
+        int tempPrice = 0;
 
 
         for (int k=0;k<keyboards.length;k++){
             for (int d =  0;d<drives.length;d++){
                 totals[totPosition]=(keyboards[k]+drives[d]);
+                System.out.println("test pos" + totPosition +" $"+ totals[totPosition]);
+                if (b-totals[totPosition]==0){ System.out.println("bail "+b); return b;}
+                if (b-totals[totPosition]>0 && b-totals[totPosition]<b-tempPrice){
+                    System.out.println("testB");
+                    tempPrice=totals[totPosition];
+                    price=totals[totPosition];
+                }
                 totPosition++;
             }
         }
 
-        for (int p=0;p<totals.length;p++){
-            if (b-totals[p]==0) {
-                return b;
-            }
-            while (tempPriceSet == 0) {
-                for (int pp = 0; pp < totals.length; pp++) {
-                    tempPriceSetLoop++;
-                    if (b - totals[pp] >= 0 && tempPriceSet == 0) {
-                        tempPrice = totals[pp];
-                        tempPriceSet = 1;
-                        break;
-                    }
-                }
-                if (tempPriceSetLoop == b+1)return -1;
-
-
-            }
-
-            if ((totals[p]<=b) && (b-totals[p]>=0) && (b-totals[p]<b-tempPrice) && (tempPriceSet ==1)){
-                tempPrice = totals[p];
-                price = totals[p];
-
-            }
-        }
-
+//        for (int p=0;p<totals.length;p++){
+//            while (tempPriceSet == 0) {
+//                for (int pp = 0; pp < totals.length; pp++) {
+//                    tempPriceSetLoop++;
+//                    if (b - totals[pp] >= 0 && tempPriceSet == 0) {
+//                        tempPrice = totals[pp];
+//                        tempPriceSet = 1;
+//                        break;
+//                    }
+//                }
+//                if (tempPriceSetLoop == b+1)return -1;
+//            }
+//            if ((totals[p]<=b) && (b-totals[p]>=0) && (b-totals[p]<b-tempPrice) && (tempPriceSet ==1)){
+//                tempPrice = totals[p];
+//                price = totals[p];
+//            }
+//        }
+        System.out.println(price);
         return price;
     }
 
@@ -67,18 +65,16 @@ public class Electronics_Shop {
 //        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
 
         //int b = Integer.parseInt(bnm[0]);
-        int b = 5;
-
+        int b = 10;
        // int n = Integer.parseInt(bnm[1]);
-        int n = 1;
-
+        int n = 3;
         //int m = Integer.parseInt(bnm[2]);
-        int m = 1;
+        int m = 3;
 
         int[] keyboards = new int[n];
-        keyboards[0] = 4;
-        //keyboards[1] = 1;
-        //keyboards[2] = 4;
+        keyboards[0] = 2;
+        keyboards[1] = 8;
+        keyboards[2] = 0;
 
 
 //        String[] keyboardsItems = scanner.nextLine().split(" ");
@@ -91,8 +87,8 @@ public class Electronics_Shop {
 
         int[] drives = new int[m];
         drives[0] =5;
-        //drives[1] = 4625;
-        //drives[2] = 8;
+        drives[1] = 1;
+        drives[2] = 15;
 
 //        String[] drivesItems = scanner.nextLine().split(" ");
 //        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
